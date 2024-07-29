fd3aad34aed95834e592d9d2b785d6b516974929
Ｌｅｏ <102800624+Leowwd@users.noreply.github.com>
2024-07-29 07:35:17 +0000

    feat: api function

--- audio_server/routes.py
+++ audio_server/routes.py
@@ -11,14 +11,23 @@ def index():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return jsonify({'error': 'No file part'})
-        file = request.files['file']
-        threshold = float(request.form['threshold'])
-        if file.filename == '':
-            return jsonify({'error': 'No selected file'})
-        if file:
-            audio_path = save_audio(file)
-            weak_phonemes, predictions, correct, diff_out, ratio = audio_service(audio_path, threshold)
+        insertValues = request.get_json()
+        uri = insertValues["uri"] # input
+        index = insertValues["index"] # input
+        threshold = insertValues["threshold"] # input
+        # if 'file' not in request.files:
+        #     return jsonify({'error': 'No file part'})
+        # file = request.files['file']
+        # threshold = float(request.form['threshold'])
+        # if file.filename == '':
+        #     return jsonify({'error': 'No selected file'})
+        # if file:
+        #     audio_path = save_audio(file)
+        #     weak_phonemes, predictions, correct, diff_out, ratio = audio_service(audio_path, threshold)
+        #     return jsonify({'weak_phonemes': weak_phonemes, 'predictions': predictions, "correct": correct, "diff": diff_out, "accuracy": ratio, "suggestion": "To be implemented"})
+
+        if uri:
+            audio_path = save_audio(uri)
+            weak_phonemes, predictions, correct, diff_out, ratio = audio_service(audio_path, threshold, index)
             return jsonify({'weak_phonemes': weak_phonemes, 'predictions': predictions, "correct": correct, "diff": diff_out, "accuracy": ratio, "suggestion": "To be implemented"})
     return render_template('upload.html')
