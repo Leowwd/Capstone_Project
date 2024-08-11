99d3fb47fb2462697d35988d0182cb15fc91f3e8
Ｌｅｏ <102800624+Leowwd@users.noreply.github.com>
2024-08-11 09:26:43 +0000

    fix: 曾慶倫耍智障

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-
-def create_app():
-    app = Flask(__name__, template_folder='templates')
-    with app.app_context():
-        import audio_server.routes
-        CORS(app)
-    return app
--- run.py
+++
@@ -1,6 +0,0 @@
-from audio_server import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
